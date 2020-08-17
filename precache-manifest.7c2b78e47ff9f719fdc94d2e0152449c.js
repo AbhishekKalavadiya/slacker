c0370ec98bdcff1181073cac87fe52b8431fe75a
Abhishek Kalavadiya <51313689+AbhishekKalavadiya@users.noreply.github.com>
2020-08-17 01:43:23 -0700

    Updates

---
+++ precache-manifest.7c2b78e47ff9f719fdc94d2e0152449c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4a72addafa1e2bd5c44a9f28180d5a",
+    "url": "/slacker/index.html"
+  },
+  {
+    "revision": "445ccab4b90fcc8a32cf",
+    "url": "/slacker/static/css/main.a1a6f1d4.chunk.css"
+  },
+  {
+    "revision": "47ee332abb78ba757e52",
+    "url": "/slacker/static/js/2.1a4dbcfe.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/slacker/static/js/2.1a4dbcfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "445ccab4b90fcc8a32cf",
+    "url": "/slacker/static/js/main.5663da2e.chunk.js"
+  },
+  {
+    "revision": "d5ea1ef19332dc30fcba",
+    "url": "/slacker/static/js/runtime-main.1a7e160f.js"
+  }
+]);
